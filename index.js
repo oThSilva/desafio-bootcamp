444f5b5aaa920d75ea548d2b2d78b38f3f7b9575
Thiago <thipereira.silva@hotmail.com>
2023-12-26 13:00:13 -0300

    refatoração

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-
 let nomeHeroi = "Seiya"
-let nivel = 5001
+let nivel = 50000
 let nivelHeroi  = ""
 
 if (nivel < 1000){
     nivelHeroi = "Ferro"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 1001 && nivel <= 2000){
+}else if (nivel <= 2000){
     nivelHeroi = "Bronze"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 2001 && nivel <= 5000){
+}else if (nivel <= 5000){
     nivelHeroi = "Prata"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 5001 && nivel <= 7000){
+}else if (nivel <= 7000){
     nivelHeroi = "Ouro"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 7001 && nivel <= 8000){
+}else if (nivel <= 8000){
     nivelHeroi = "Platina"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 8001 && nivel <= 9000){
+}else if (nivel <= 9000){
     nivelHeroi = "Ascendente"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 9001 && nivel <= 10000){
+}else if (nivel <= 10000){
     nivelHeroi = "Imortal"
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
-}else if (nivel >= 10001){
-    nivelHeroi = ""
-    console.log("O heroi de nome " + nomeHeroi + " esta no nivel de " + nivelHeroi)
+}else{
+    nivelHeroi = "Radiante"
 }
 
-
+console.log(`O heroi de nome ${nomeHeroi} está no nivel de ${nivelHeroi}`)
 
 
 
